31b741267cc8cebc50499b083effd4ddadbd0a3f
DucMai <DucMai@fortna.com>
2025-12-17 14:25:06 +0700

    change aff id

--- src/constants/affiliate.js
+++ src/constants/affiliate.js
@@ -1,4 +1,4 @@
 export const SHOPEE_WHITELIST_DOMAINS = ['shope.ee', 'shopee.vn', 's.shopee.vn']
-export const AFFILIATE_ID = '17321190424'
+export const AFFILIATE_ID = '17354980366'
 export const CLEAN_LINK_ENDPOINT = 'https://clean-product-link.ducmami.workers.dev/'
 
